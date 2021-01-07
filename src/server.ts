6b9464c214b96a7f427181787cec0add31db36f6
aalzate <aalzate@sonatype.com>
2021-01-06 22:51:33 -0500

    Add authentication

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
-import express from 'express';
+import express , {Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import * as jwt from 'jsonwebtoken';
+import { NextFunction } from 'connect';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { config } from './config/config';
 
 (async () => {
 
@@ -14,7 +17,9 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.use(bodyParser.json());
 
     // endpoint to filter an image from a public url.
-  app.get('/filteredimage', async ( req, res ) => {
+  app.get('/filteredimage', 
+  requireAuth,
+  async ( req: Request, res: Response ) => {
     try{
       //Get image_url query param
       const image_url = req.query.image_url;
@@ -41,10 +46,30 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
+  function requireAuth(req: Request, res: Response, next: NextFunction) {
+    if (!req.headers || !req.headers.authorization){
+        return res.status(401).send({ message: 'No authorization headers.' });
+    }
+    
+    const token_bearer = req.headers.authorization.split(' ');
+    if(token_bearer.length != 2){
+        return res.status(401).send({ message: 'Malformed token.' });
+    }
+    
+    const token = token_bearer[1];
+
+    return jwt.verify(token, config.jwt.secret, (err, decoded) => {
+      if (err) {
+        return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+      }
+      return next();
+    });
+}
+
 
   // Start the Server
   app.listen( port, () => {
